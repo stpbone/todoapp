6d578e163f9e80dd616f3b3cf626c411dc3df19c
Simon Bone <32798419+stpbone@users.noreply.github.com>
2018-10-02 15:36:48 +0100

    App developed when learning Django
    To do list app created using Django framework and MySQL databse

---
+++ todolist/todos/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class TodosConfig(AppConfig):
+    name = 'todos'
+
+[
+    # ...
+    "staticfiles",
+]
+
+
---
+++ todolist/todos/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from datetime import datetime
+
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at =  models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return self.title
+        
